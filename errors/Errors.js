309056e9126b5b0d7640c5c6bc97f69ae73b6c84
Diego Castillo <61103323+LearnLab@users.noreply.github.com>
2020-12-29 19:36:51 -0500

    Add error handler

---
+++ errors/Errors.js
@@ -0,0 +1,22 @@
+const Errors = {
+    "400": {
+        status: 400,
+        title: "Bad Request"
+    },
+    "406": {
+        status: 406,
+        title: "Not Acceptable"
+    },
+    "415": {
+        status: 415,
+        title: "Unsupported Media Type"
+    },
+    "500": {
+        status: 500,
+        title: "Unknown Internal Server Error"
+    }
+};
+
+module.exports = {
+    Errors
+}
