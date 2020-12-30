ac336c37facfdb53fbdebc261d215fddcabc6983
Diego Castillo <61103323+LearnLab@users.noreply.github.com>
2020-12-29 19:54:58 -0500

    Import Require middlewares from API router

--- routes/api.js
+++ routes/api.js
@@ -1,19 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
-const usersController = require('../controllers/users');
+const { UsersController } = require('../controllers/users');
+const { RequireTopLevel } = require('../middleware/RequireTopLevel');
+const { RequireUserType } = require('../middleware/RequireUserType');
+const { RequireEmailAttribute } = require('../middleware/RequireEmailAttribute');
+const { RequireUsernameAttribute } = require('../middleware/RequireUsernameAttribute');
+const { RequirePasswordsAttribute } = require('../middleware/RequirePasswordsAttribute');
+
 const authController = require('../controllers/authentication');
 
 router.route('/authentication')
       .post(authController.authentication);
 
 router.route('/users')
-      .get(usersController.getAllUsers)
-      .post(usersController.createUser);
+      .get(UsersController.index);
+
+router.post('/users', RequireTopLevel)
+      .post('/users', UsersController.create);
 
 router.route('/users/:username')
-      .get(usersController.getUser)
-      .put(usersController.updateUser)
-      .delete(usersController.deleteUser);
+      .get(UsersController.show)
+      .delete(UsersController.delete);
+
+router.put('/users/:username', RequireTopLevel)
+      .put('/users/:username', UsersController.update);
 
 module.exports = router;
